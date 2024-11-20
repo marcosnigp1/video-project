aa9b03c0988089daef56f3bb25c82daf3dba5373
Marcos Hernández <126048022+marcosnigp1@users.noreply.github.com>
2024-11-21 00:32:06 +0400

    Fixed bug of video not playing with Chromium

--- sketch.js
+++ sketch.js
@@ -24,6 +24,7 @@ function setup() {
   //Preparing UI class.
   ui = new UI(width * 0.45, height * 0.9, 2);
   windowResized();
+  video.attribute("preload", "true");
   video.attribute("playsinline", "true");
 }
 
@@ -74,9 +75,9 @@ function mousePressed() {
     mouseY < ui.position.y * 1 &&
     ui.decision_moment == 1
   ) {
+    ui.play_video(); //First play the video, THEN JUMP to the seconds. Chrome has troubles understanding what to do if the time skip is done first...
     ui.jump_to_seconds(ui.time_skip_a);
     ui.decision_moment = 0;
-    ui.play_video();
   } else if (
     mouseX > ui.position.x * 1.3 &&
     mouseX < ui.position.x * 1.9 &&
@@ -84,9 +85,9 @@ function mousePressed() {
     mouseY < ui.position.y * 1 &&
     ui.decision_moment == 1
   ) {
+    ui.play_video();
     ui.jump_to_seconds(ui.time_skip_b);
     ui.decision_moment = 0;
-    ui.play_video();
   }
 }
 
